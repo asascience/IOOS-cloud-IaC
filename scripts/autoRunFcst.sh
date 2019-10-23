f59aca07228b01897385c46b42782bcb6bbf4b72
Patrick Tripp <patrick.tripp@rpsgroup.com>
2019-10-23 18:58:54 +0000

    cant put Ntile at end of .in

--- scripts/autoRunFcst.sh
+++ scripts/autoRunFcst.sh
@@ -5,9 +5,6 @@ startdir=$PWD
 module load produtil
 module load gcc
 
-# This is needed for Intel MPI 2019
-export I_MPI_FABRICS=ofi
-
 COMDIR=/noscrub/com/nos
 NOSDIR=/save/nosofs.v3.1.9.1
 
@@ -21,16 +18,24 @@ echo "Fetching ICs for $prevcycle"
 cdate=$(echo $prevcycle | cut -c1-8)
 cyc=$(echo $prevcycle | cut -c9-10)
 
+romsin=nos.cbofs.forecast.${cdate}.t${cyc}z.in
+romssave=nos.cbofs.forecast.${cdate}.t${cyc}z.in.save
+
+if [ -s $COMDIR/cbofs.$cdate/$romsin ] ; then
+  rm $COMDIR/cbofs.$cdate/$romsin
+fi
+
 # Get the ICs
 ./getICs_cbofs.sh $cdate $cyc
 
 # Change the roms.in file, tiles and run length
 cd $COMDIR/cbofs.$cdate
 
-romsin=nos.cbofs.forecast.${cdate}.t${cyc}z.in
-romssave=nos.cbofs.forecast.${cdate}.t${cyc}z.in.save
 cp -p $romsin $romssave
 
+# NtileI == 10
+# NtileJ == 14
+# NTIMES == 34560
 
 Itiles=8
 Jtiles=12
@@ -38,15 +43,12 @@ NTIMES=720   # 6 hour
 #NTIMES=120   # 1 hour
 
 # Replace tiles and ntimes with custom setting
-# Safest and most reusable way, just delete any matching lines
+# Order seems to be important
 
-sed -i '/NtileI/d' $romsin
-sed -i '/NtileJ/d' $romsin
-echo "NtileI == $Itiles" >> $romsin
-echo "NtileJ == $Jtiles" >> $romsin
+sed -i "s/NtileI == 10/NtileI == $Itiles/" $romsin
+sed -i "s/NtileJ == 14/NtileJ == $Jtiles/" $romsin
 
-sed -i '/NTIMES/d' $romsin
-echo "NTIMES == $NTIMES" >> $romsin
+sed -i "s/NTIMES == 34560/NTIMES == $NTIMES/" $romsin
 
 # Launch the forecast
 export NPP=$(($Itiles * $Jtiles))
@@ -54,11 +56,8 @@ export NPP=$(($Itiles * $Jtiles))
 cd $NOSDIR/jobs
 ./fcstrun.sh $cdate $cyc
 
-
 # Copy results to S3 bucket using aws-cli
 cd $startdir
 ./copyResults2S3.sh $COMDIR $cdate $cyc
 
-# Send a shutdown signal
-# sudo shutdown now
 
