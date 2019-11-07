4b03a7bd9fddecb2cc480a7d5ccead3451b8fd84
Patrick Tripp <patrick.tripp@rpsgroup.com>
2019-11-07 07:56:33 -0500

    New CFN Template and getNomadsProd.sh update

--- scripts/getNomadsProd.sh
+++ scripts/getNomadsProd.sh
@@ -2,23 +2,39 @@
 
 # Retrieve 48 hour forecast from NOAA
 
-#nos.cbofs.fields.f001.20191021.t00z.nc 
-NOMADS=https://nomads.ncep.noaa.gov/pub/data/nccf/com/nos/prod/cbofs.20191021
+if [ $# -lt 3 ] ; then
+  echo "Usage: $0 cbofs|ngofs|etc. yyyymmdd cycle [directory]"
+  exit 1
+fi
 
-hlist='01 02 03 04 05 06 07 08 09'
+
+ofs=$1
+cdate=$2
+cyc=$3
+if [ $# -gt 3 ]; then
+  dest=$4
+  mkdir -p $dest
+  cd $dest
+fi
+
+#nos.$ofs.fields.f001.$cdate.t${cyc}z.nc 
+NOMADS=https://nomads.ncep.noaa.gov/pub/data/nccf/com/nos/prod/$ofs.$cdate
+
+#hlist='01 02 03 04 05 06 07 08 09'
+hlist='01 06 12 18 24 36 48'
 
 for hh in $hlist
 do
-  wget -nc $NOMADS/nos.cbofs.fields.f0$hh.20191021.t00z.nc
+  wget -nc $NOMADS/nos.$ofs.fields.f0$hh.$cdate.t${cyc}z.nc
 done
 
 
-hh=10
-while [ $hh -le 48 ] ; do
-  wget -nc $NOMADS/nos.cbofs.fields.f0$hh.20191021.t00z.nc
-  hh=$(($hh+1))
-done
+#hh=10
+#while [ $hh -le 48 ] ; do
+#  wget -nc $NOMADS/nos.$ofs.fields.f0$hh.$cdate.t${cyc}z.nc
+#  hh=$(($hh+1))
+#done
 
-wget -nc $NOMADS/nos.cbofs.forecast.20191021.t00z.in
-wget -nc $NOMADS/nos.cbofs.forecast.20191021.t00z.log
+wget -nc $NOMADS/nos.$ofs.forecast.$cdate.t${cyc}z.in
+wget -nc $NOMADS/nos.$ofs.forecast.$cdate.t${cyc}z.log
 
