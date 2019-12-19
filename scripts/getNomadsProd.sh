0b65cae6cb817f0b9d404d095cf6933ddbe5ef98
Patrick Tripp <patrick.tripp@rpsgroup.com>
2019-12-19 08:18:08 -0500

    minor script updates

--- scripts/getNomadsProd.sh
+++ scripts/getNomadsProd.sh
@@ -3,38 +3,43 @@
 # Retrieve 48 hour forecast from NOAA
 
 if [ $# -lt 3 ] ; then
-  echo "Usage: $0 cbofs|ngofs|etc. yyyymmdd cycle [destination path]"
+  echo "Usage: $0 cbOFS|ngOFS|etc. yyyymmdd hh [destination path]"
   exit 1
 fi
 
 
-ofs=$1
-cdate=$2
-cyc=$3
+OFS=$1
+CDATE=$2
+CYC=$3
 if [ $# -gt 3 ]; then
   dest=$4
   mkdir -p $dest
   cd $dest
 fi
 
-#nos.$ofs.fields.f001.$cdate.t${cyc}z.nc 
-NOMADS=https://nomads.ncep.noaa.gov/pub/data/nccf/com/nos/prod/$ofs.$cdate
+#nos.$OFS.fields.f001.$CDATE.t${CYC}z.nc 
+NOMADS=https://nomads.ncep.noaa.gov/pub/data/nccf/com/nos/prod/$OFS.$CDATE
 
 #hlist='01 02 03 04 05 06 07 08 09'
 hlist='01 06 12 18 24 36 48'
 
 for hh in $hlist
 do
-  wget -nc $NOMADS/nos.$ofs.fields.f0$hh.$cdate.t${cyc}z.nc
+  wget -nc $NOMADS/nos.$OFS.fields.f0$hh.$CDATE.t${CYC}z.nc
 done
+
+if [[ $OFS == "ngofs" ]] ; then
+  wget -nc $NOMADS/nos.ngofs.nestnode.*.forecast.$CDATE.t${CYC}z.nc
+fi
+
 
 
 #hh=10
 #while [ $hh -le 48 ] ; do
-#  wget -nc $NOMADS/nos.$ofs.fields.f0$hh.$cdate.t${cyc}z.nc
+#  wget -nc $NOMADS/nos.$OFS.fields.f0$hh.$CDATE.t${CYC}z.nc
 #  hh=$(($hh+1))
 #done
 
-wget -nc $NOMADS/nos.$ofs.forecast.$cdate.t${cyc}z.in
-wget -nc $NOMADS/nos.$ofs.forecast.$cdate.t${cyc}z.log
+wget -nc $NOMADS/nos.$OFS.forecast.$CDATE.t${CYC}z.in
+wget -nc $NOMADS/nos.$OFS.forecast.$CDATE.t${CYC}z.log
 
