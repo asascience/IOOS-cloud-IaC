55aea3e9f2e988bd13f68d365ae05e4842702ffa
Kenny Ells <kenny.ells@RI-VSQ-4AMLVDQ.local>
2020-01-12 22:50:12 -0500

    Add plotting routines and post_roms.py script

---
+++ python/plotting/tasks.py
@@ -0,0 +1,101 @@
+import os
+import pyproj
+import cmocean
+import netCDF4
+import PIL.Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+from prefect import task
+from prefect.engine.signals import SKIP
+
+import tile
+
+
+@task
+def plot_roms(ncfile: str, target: str, varname: str, crop: bool = False, zoom: int = 10):
+    '''Plot roms output'''
+
+    EPSG3857 = pyproj.Proj('EPSG:3857')
+    TILE3857 = tile.Tile3857()
+
+    with netCDF4.Dataset(ncfile) as nc:
+
+        # Extract spatial variables
+        lon = nc.variables['lon_rho'][:]
+        lat = nc.variables['lat_rho'][:]
+        msk = nc.variables['mask_rho'][:]
+        lo,la = EPSG3857(lon,lat) # project EPSG:4326 to EPSG:3857 (web mercator)
+
+        # temporal
+        time = nc.variables['ocean_time']
+        time = netCDF4.num2date(time[:], units=time.units)
+
+        # data
+        data = nc.variables[varname]
+        d = data[:]
+
+        # image size based on tiles
+        mla = la[msk == 0]
+        mlo = lo[msk == 0]
+        lrt = TILE3857.tile(mlo.max(), mla.min(), zoom)
+        lrb = TILE3857.bounds(*lrt)
+        ult = TILE3857.tile(mlo.min(), mla.max(), zoom)
+        ulb = TILE3857.bounds(*ult)
+        ntx = lrt[0] - ult[0] + 1 # number of tiles in x
+        nty = lrt[1] - ult[1] + 1 # number of tiles in y
+
+        # loop times in file
+        for t in range(len(time)):
+
+            d = d[t, :] # select time
+
+            # check for vertical coordinate
+            if d.ndim > 2:
+                d = d[-1,:] # surface is last in ROMS
+
+            # apply mask
+            d = np.ma.masked_where(msk == 0, d)
+
+            # pcolor uses surrounding points, if any are masked, mask this cell
+            #   see https://matplotlib.org/api/_as_gen/matplotlib.pyplot.pcolor.html
+            d[:-1,:] = np.ma.masked_where(msk[1:,:] == 0, d[:-1,:])
+            d[:,:-1] = np.ma.masked_where(msk[:,1:] == 0, d[:,:-1])
+
+            # image size/resolution
+            dpi = 256
+            height = nty * dpi
+            width  = ntx * dpi
+
+            fig = plt.figure(dpi=dpi, facecolor='none', edgecolor='none')
+            fig.set_alpha(0)
+            fig.set_figheight(height/dpi)
+            fig.set_figwidth(width/dpi)
+
+            ax = fig.add_axes([0., 0., 1., 1.], xticks=[], yticks=[])
+            ax.set_axis_off()
+
+            # pcolor
+            pcolor = ax.pcolor(lo, la, d, cmap=plt.get_cmap('viridis'), edgecolors='k', linewidth=0.05)
+
+            ax.set_frame_on(False)
+            ax.set_clip_on(False)
+            ax.set_position([0, 0, 1, 1])
+
+            # set limits based on tiles
+            ax.set_xlim(ulb[0], lrb[2])
+            ax.set_ylim(lrb[1], ulb[3])
+
+            # File output
+            filename = ncfile.split('/')[-1][:-3]
+            fig.savefig(f'{target}/{filename}_{varname}.png', dpi=dpi, bbox_inches='tight', pad_inches=0.0, transparent=True)
+
+            plt.close(fig)
+
+            if crop:
+                with PIL.Image.open(filename) as im:
+                    zeros = PIL.Image.new('RGBA', im.size)
+                    im = PIL.Image.composite(im, zeros, im)
+                    bbox = im.getbbox()
+                    crop = im.crop(bbox)
+                    crop.save(filename, optimize=True)
---
+++ python/post_roms.py
@@ -0,0 +1,32 @@
+import os
+
+from prefect import Flow, task, unmapped
+from prefect.engine.executors import DaskExecutor
+from dask.distributed import Client
+
+from plotting.tasks import plot_roms
+
+
+def main():
+
+    SOURCE = os.path.abspath('/Users/kenny.ells/data/dubai_his_arg.20191226')
+    TARGET = os.path.abspath('tmp')
+    FILES = sorted([os.path.join(SOURCE, f) for f in os.listdir(SOURCE)])
+    FILES = FILES[:5]
+
+    with Flow('plotting') as flow:
+        plot_roms.map(ncfile=FILES, target=unmapped(TARGET), varname=unmapped('temp'))
+
+    # address_tcp = 'tcp://10.90.69.73:8786'
+    # print(address_tcp)
+    # executor = DaskExecutor(address=address_tcp, local_processes=True, debug=True)
+
+    client = Client()
+    print(client)
+    address_tcp = client.scheduler.address
+    executor = DaskExecutor(address=address_tcp)
+
+    flow.run(executor=executor)
+
+if __name__=='__main__':
+    main()
